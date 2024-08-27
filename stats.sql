bd48e76701a5025bfb640109c7ae02358ec800a2
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-08-27 12:07:48 +0530

    sql stats update

--- stats.sql
+++ stats.sql
@@ -1,1 +1,32 @@
 SHOW block_size;
+
+SHOW block_size;
+
+-- Query to show compression settings
+SELECT name, setting 
+FROM pg_settings 
+WHERE name LIKE '%compress%';
+
+-- Query to show indexes on all partitions of a partitioned table
+WITH RECURSIVE inheritance_tree AS (
+    SELECT c.oid, c.relname, c.relispartition
+    FROM pg_class c
+    WHERE c.relname = 'wabachats_partitioned'
+    UNION ALL
+    SELECT c.oid, c.relname, c.relispartition
+    FROM pg_class c
+    JOIN pg_inherits i ON c.oid = i.inhrelid
+    JOIN inheritance_tree it ON i.inhparent = it.oid
+    WHERE c.relispartition
+)
+SELECT 
+    it.relname AS partition_name,
+    i.relname AS index_name,
+    pg_get_indexdef(i.oid) AS index_definition
+FROM inheritance_tree it
+JOIN pg_index ix ON it.oid = ix.indrelid
+JOIN pg_class i ON ix.indexrelid = i.oid
+JOIN pg_am am ON i.relam = am.oid
+WHERE it.relispartition
+ORDER BY it.relname, i.relname;
+
